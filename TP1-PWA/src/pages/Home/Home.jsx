f568502c2a781e43a15f5e3de7cbc94e8833b467
MGIC12 <mager.infante@gmail.com>
2026-04-03 15:25:20 -0300

    Agregadas las carpetas que pide el punto 3

---
+++ TP1-PWA/src/pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./Home.module.css";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className={`${styles.centrado} ${styles.titulo}`}>Home</h1>
+    </div>
+  );
+}
