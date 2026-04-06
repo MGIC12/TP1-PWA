6bc5f8d37bdea6be91a6af37fc61999c5672e4c2
palomaStucke <paloma.stucke@est.fi.uncoma.edu.ar>
2026-04-06 18:35:46 -0300

    Agrego componente Boton

---
+++ TP1-PWA/src/Components/Boton/Boton.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Boton() {
+ const contenido = "HOLA"
+  return (
+    <button name="Boton">
+        {contenido}
+    </button>
+  );
+}
--- TP1-PWA/src/pages/Home/Home.jsx
+++ TP1-PWA/src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import styles from "./Home.module.css";
 import Titulo from "../../Components/Titulo/Titulo";
+import Boton from "../../Components/Boton/Boton";
 
 export default function Home() {
   return (
     <div>
       <Titulo />
       <h1 className={`${styles.centrado} ${styles.titulo}`}>Home</h1>
+      <Boton/>
     </div>
   );
 }
