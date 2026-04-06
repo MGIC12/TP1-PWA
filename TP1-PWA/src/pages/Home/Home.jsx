524983d26016850ee249e6db4fc9d316109b9278
MGIC12 <mager.infante@gmail.com>
2026-04-06 18:10:11 -0300

    Cambios para el merge y comenzar a trabajar

--- TP1-PWA/src/pages/Home/Home.jsx
+++ TP1-PWA/src/pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./Home.module.css";
-import Titulo from "../../Components/Titulo";
+import Titulo from "../../Components/Titulo/Titulo";
 
 export default function Home() {
   return (
