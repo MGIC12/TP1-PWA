bd11e68fe8a88cdfc7fc9d63f21783f8a5d7c2c5
MGIC12 <mager.infante@gmail.com>
2026-04-08 13:16:46 -0300

    Agregado el componente Modal, con sus respectivos estilos e implementación en Home.jsx

--- TP1-PWA/src/pages/Home/Home.jsx
+++ TP1-PWA/src/pages/Home/Home.jsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Home.module.css";
 import Titulo from "../../Components/Titulo/Titulo";
+import Modal from "../../Components/Modal/Modal";
 
 export default function Home() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   return (
-    <div>
-      <Titulo texto="Este es el título" />
-      <h1 className={`${styles.centrado} ${styles.titulo}`}>Home</h1>
-    </div>
+    <>
+      <div>
+        <Titulo texto="Este es el título" />
+        <h1 className={`${styles.centrado} ${styles.titulo}`}>Home</h1>
+      </div>
+      <div>
+        <button onClick={() => setIsModalOpen(true)}>
+          Botón para abrir el modal
+        </button>
+
+        <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
+      </div>
+    </>
   );
 }
