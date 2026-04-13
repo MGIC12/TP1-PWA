e5c5953a197fec29a82a8fe038f47ce685772127
palomaStucke <paloma.stucke@est.fi.uncoma.edu.ar>
2026-04-13 15:35:01 -0300

    Componente Columna con sus estilos

---
+++ TP1-PWA/src/Components/Columna/Columna.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styles from "";
+
+export default function Columna({  titulo, items =[], renderItem }) {
+    return(
+        <div className={styles.columna}>
+            <h2 className={styles.titulo}>{titulo}</h2>
+            {items.length === 0 ? (
+                <p className={styles.empty}>No hay contenido</p>
+            ) : (
+                <ul className={styles.lista}>
+                    {items.map(( item, index ) => (
+                        <li key={index} className={styles.item}>
+                            {renderItem ? renderItem(item) : item.titulo}
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </div>
+    );
+}
