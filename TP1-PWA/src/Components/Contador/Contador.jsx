d23276524f25e11887d69a525cb4cc052edbfb75
palomaStucke <paloma.stucke@est.fi.uncoma.edu.ar>
2026-04-13 16:05:25 -0300

    Componente contador con sus estilos

---
+++ TP1-PWA/src/Components/Contador/Contador.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import styles from "./Contador.module.css";
+
+export default function Contador({lista, items = []}) {
+    const totalLista= items.length;
+
+    //Agrupo por género
+    const conteoPorGenero = items.reduce((acc, item) => {
+        acc[item.genero] = (acc[item.genero] || 0) +1;
+        return acc;
+    }, {});
+
+    const generos = Object.entries(conteoPorGenero); //[ ["accion",2] , ["drama",1] ...]
+    return (
+        <div className={styles.contenedor}>
+            <h2 className= {styles.titulo}>Resumen: {lista}</h2>
+            <p className= {styles.total}>Total: {totalLista}</p>
+
+            {generos.length === 0 ? (
+                <p className={styles.empty}>No hay contenido</p>
+            ) : (
+                <ul className={styles.lista}>
+                    {generos.map(([genero, cantidad]) => (
+                        <li key={genero} className={styles.item}>
+                            <span className={styles.genero}>{genero}</span>
+                            <span className={styles.valor}>{cantidad}</span>
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </div>
+    );
+}
