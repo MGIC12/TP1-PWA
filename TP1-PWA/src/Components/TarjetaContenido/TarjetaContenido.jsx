2017a94384a8a175c40664caffa79ca52f9261e5
matias bacsay <149085169+tebacs@users.noreply.github.com>
2026-04-14 01:22:03 -0300

    Agregar funcionalidad de eliminación y actualizar estilos de botones en el componente TarjetaContenido

--- TP1-PWA/src/Components/TarjetaContenido/TarjetaContenido.jsx
+++ TP1-PWA/src/Components/TarjetaContenido/TarjetaContenido.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import styles from "./TarjetaContenido.module.css"
 import Etiqueta from '../Etiqueta/Etiqueta'
+import Boton from '../Boton/Boton'
 
-export default function TarjetaContenido({ objContenido }) {
-    const generos = objContenido.colGeneros.map((genero) => {
-        return (<Etiqueta key={genero} nombre={genero}></Etiqueta>);
-    });
-
+export default function TarjetaContenido({ objContenido, onDelete }) {
+    const estado = objContenido.estado; // "vista" o "por-ver"
+    
     return (
         <div className={styles.TarjetaContenido}>
             <div className={styles.contenedor}>
@@ -26,20 +25,27 @@ export default function TarjetaContenido({ objContenido }) {
 
                     {/* Etiquetas de género */}
                     <div className={styles.generos}>
-                        {generos}
+                        <Etiqueta nombre={objContenido.genero} />
                     </div>
 
                 </div>
                 <div className={styles.acciones}>
-                    <button className={styles.btnBorrar} title="Borrar">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-trash-x"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M4 7h16" /><path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" /><path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" /><path d="M10 12l4 4m0 -4l-4 4" /></svg>
-                    </button>
-                    <button className={styles.btnEditar} title="Editar">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-pencil"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M4 20h4l10.5 -10.5a2.828 2.828 0 1 0 -4 -4l-10.5 10.5v4" /><path d="M13.5 6.5l4 4" /></svg>
-                    </button>
-                    <button className={styles.btnVisto} title="Marcar como visto">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-eye"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" /><path d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6" /></svg>
-                    </button>
+                
+                        <Boton
+                                    className={styles.btnBorrar}
+                                    onClick={() => onDelete(objContenido.id, estado)}
+                                    />
+                    
+                    
+                    <Boton
+                                  className={styles.btnEditar}
+                                  type="submit"
+                                />
+                    
+                    <Boton
+                                  className={estado === "vista" ? styles.btnVista : styles.btnPorVer}
+                                  type="submit"
+                                />
                 </div>
             </div>
                 {/* Rating y Tipo en la base de la tarjeta */}
